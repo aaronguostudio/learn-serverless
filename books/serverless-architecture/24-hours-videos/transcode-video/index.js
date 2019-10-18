f585b21eec85a693518c224b70a9d59a747ad7bf
aaronguostudio <aaronguostudio@gmail.com>
2019-10-18 09:16:37 -0600

    update transcoder lambda config

--- books/serverless-architecture/24-hours-videos/transcode-video/index.js
+++ books/serverless-architecture/24-hours-videos/transcode-video/index.js
@@ -18,17 +18,21 @@ exports.handler = (event, context, callback) => {
       Key: sourceKey
     },
     Outputs: [
+      // {
+      //     Key: outputKey + '-1080p' + '.mp4',
+      //     PresetId: '1351620000001-000001' //Generic 1080p
+      // },
+      // {
+      //     Key: outputKey + '-720p' + '.mp4',
+      //     PresetId: '1351620000001-000010' //Generic 720p
+      // },
+      // {
+      //     Key: outputKey + '-web-720p' + '.mp4',
+      //     PresetId: '1351620000001-100070' //Web Friendly 720p
+      // }
       {
-          Key: outputKey + '-1080p' + '.mp4',
-          PresetId: '1351620000001-000001' //Generic 1080p
-      },
-      {
-          Key: outputKey + '-720p' + '.mp4',
-          PresetId: '1351620000001-000010' //Generic 720p
-      },
-      {
-          Key: outputKey + '-web-720p' + '.mp4',
-          PresetId: '1351620000001-100070' //Web Friendly 720p
+          Key: outputKey + '-web-480p' + '.mp4',
+          PresetId: '1351620000001-000020'
       }
     ]
   }
